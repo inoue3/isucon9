f5c684ba16a510c818e5d56eaf57771b02d41105
shin.katsumaru <shin.katsumaru@supership.jp>
2019-09-08 14:01:50 +0900

    dbアクセス不必要

--- webapp/go/main.go
+++ webapp/go/main.go
@@ -421,16 +421,11 @@ func getCategoryByID(q sqlx.Queryer, categoryID int) (category Category, err err
 }
 
 func getConfigByName(name string) (string, error) {
-	config := Config{}
-	err := dbx.Get(&config, "SELECT * FROM `configs` WHERE `name` = ?", name)
-	if err == sql.ErrNoRows {
-		return "", nil
+	if name == "payment_service_url" {
+		return "https://payment18.isucon9q.catatsuy.org", nil
+	} else {
+		return "https://shipment18.isucon9q.catatsuy.org", nil
 	}
-	if err != nil {
-		log.Print(err)
-		return "", err
-	}
-	return config.Val, err
 }
 
 func getPaymentServiceURL() string {
